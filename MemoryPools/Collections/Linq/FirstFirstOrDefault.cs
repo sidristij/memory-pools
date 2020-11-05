15a230e73a46a237a71ce95a6c7f2f543abfb031
Stanislav Sidristij <sunex.development@gmail.com>
2020-11-05 19:12:56 +0300

    First() / FirstOrDefault()

---
+++ MemoryPools/Collections/Linq/FirstFirstOrDefault.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace MemoryPools.Collections.Linq
+{
+    public static partial class EnumerableEx
+    {
+        public static T First<T>(this IPoolingEnumerable<T> source)
+        {
+            var enumerator = source.GetEnumerator();
+            var hasItems = enumerator.MoveNext();
+            if (!hasItems)
+            {
+                throw new InvalidOperationException("Sequence is empty");
+            }
+            var element = enumerator.Current;
+            enumerator.Dispose();
+            return element;
+        }
+        
+        public static T First<T>(this IPoolingEnumerable<T> source, Func<T, bool> condition)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (condition(enumerator.Current))
+                {
+                    var item = enumerator.Current;
+                    enumerator.Dispose();
+                    return item;
+                }
+            }
+            enumerator.Dispose();
+            throw new InvalidOperationException("Sequence is empty");
+        }
+		
+        public static T First<T, TContext>(this IPoolingEnumerable<T> source, TContext context, Func<TContext, T, bool> condition) where TContext : struct
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (condition(context, enumerator.Current))
+                {
+                    var item = enumerator.Current;
+                    enumerator.Dispose();
+                    return item;
+                }
+            }
+            enumerator.Dispose();
+            throw new InvalidOperationException("Sequence is empty");
+        }
+        
+        public static T FirstOrDefault<T>(this IPoolingEnumerable<T> source)
+        {
+            var enumerator = source.GetEnumerator();
+            var hasItem = enumerator.MoveNext();
+
+            var item= hasItem ? enumerator.Current : default;
+            enumerator.Dispose();
+
+            return item;
+        }
+        
+        public static T FirstOrDefault<T>(this IPoolingEnumerable<T> source, Func<T, bool> condition)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (condition(enumerator.Current))
+                {
+                    var elem = enumerator.Current;
+                    enumerator.Dispose();
+                    return elem;
+                }
+            }
+            enumerator.Dispose();
+            return default;
+
+        }
+		
+        public static T FirstOrDefault<T, TContext>(this IPoolingEnumerable<T> source, TContext context, Func<TContext, T, bool> condition) where TContext : struct
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                if (condition(context, enumerator.Current))
+                {
+                    var elem = enumerator.Current;
+                    enumerator.Dispose();
+                    return elem;
+                }
+            }
+            enumerator.Dispose();
+            return default;
+        }
+    }
+}
