d662fde320fdb9a5e60596dbd11cbeaa88147304
Stanislav Sidristij <sunex.development@gmail.com>
2020-11-09 12:59:28 +0300

    .Union()

---
+++ MemoryPools/Collections/Linq/Union.Enumerable.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using MemoryPools.Collections.Specialized;
+
+namespace MemoryPools.Collections.Linq
+{
+    internal class UnionExprEnumerable<T> : IPoolingEnumerable<T>
+    {
+        private int _count;
+        private PoolingDictionary<T, int> _src;
+
+        public UnionExprEnumerable<T> Init(PoolingDictionary<T, int> src)
+        {
+            _src = src;
+            _count = 0;
+            return this;
+        }
+
+        public IPoolingEnumerator<T> GetEnumerator()
+        {
+            _count++;
+            return Pool.Get<UnionExprEnumerator>().Init(this, _src.GetEnumerator());
+        }
+
+        private void Dispose()
+        {
+            if(_count == 0) return;
+            _count--;
+            if (_count == 0)
+            {
+                _src?.Dispose();
+                Pool.Return(_src);
+                _src = default;
+
+                Pool.Return(this);
+            }
+        }
+        internal class UnionExprEnumerator : IPoolingEnumerator<T>
+        {
+            private UnionExprEnumerable<T> _parent;
+            private IPoolingEnumerator<KeyValuePair<T, int>> _src;
+            
+            public UnionExprEnumerator Init(UnionExprEnumerable<T> parent, IPoolingEnumerator<KeyValuePair<T, int>> src)
+            {
+                _src = src;
+                _parent = parent;
+                return this;
+            }
+
+            public bool MoveNext() => _src.MoveNext();
+
+            public void Reset() => _src.Reset();
+
+            object IPoolingEnumerator.Current => Current;
+
+            public T Current => _src.Current.Key;
+
+            public void Dispose()
+            {
+                _src?.Dispose();
+                _src = null;
+                
+                _parent?.Dispose();
+                _parent = default;
+                
+                Pool.Return(this);
+            }
+        }
+        IPoolingEnumerator IPoolingEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ MemoryPools/Collections/Linq/Union.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MemoryPools.Collections.Specialized;
+
+namespace MemoryPools.Collections.Linq
+{
+    public static partial class PoolingEnumerable
+    {
+        public static IPoolingEnumerable<T> Union<T>(this IPoolingEnumerable<T> source, IPoolingEnumerable<T> second)
+        {
+            var set = Pool.Get<PoolingDictionary<T, int>>().Init(0);
+            foreach (var item in source) set[item] = 1;
+            foreach (var item in second) set[item] = 1;
+            
+            return Pool.Get<UnionExprEnumerable<T>>().Init(set);
+        } 
+        
+        public static IPoolingEnumerable<T> Union<T>(this IPoolingEnumerable<T> source, IPoolingEnumerable<T> second, IEqualityComparer<T> comparer)
+        {
+            var set = Pool.Get<PoolingDictionary<T, int>>().Init(0, comparer);
+            foreach (var item in source) set[item] = 1;
+            foreach (var item in second) set[item] = 1;
+            
+            return Pool.Get<UnionExprEnumerable<T>>().Init(set);
+        }
+    }
+}
