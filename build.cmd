9d596bef2bb1f15f8c8610f4399f33222c4c40fd
Stanislav Sidristij <sunex.development@gmail.com>
2021-04-23 14:10:50 +0300

    build and pulish ver through

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
-dotnet pack MemoryPools\MemoryPools.csproj -o MemoryPools\bin\Release -c Release /p:Version=1.1.3.1 
-dotnet pack MemoryPools.Collections\MemoryPools.Collections.csproj -o MemoryPools.Collections\bin\Release -c Release /p:Version=1.1.3.1
+SET PACKAGE_VERSION=1.1.3.5
 
-dotnet nuget push MemoryPools\bin\Release\MemoryPools.1.1.3.1.nupkg -k oy2cthywk3rsieisauhabiru7magfuue3nm4xr4illfuuq -s https://api.nuget.org/v3/index.json
-dotnet nuget push MemoryPools.Collections\bin\Release\MemoryPools.Collections.1.1.3.1.nupkg -k oy2cthywk3rsieisauhabiru7magfuue3nm4xr4illfuuq -s https://api.nuget.org/v3/index.json
+dotnet pack MemoryPools\MemoryPools.csproj -o MemoryPools\bin\Release -c Release /p:Version=%PACKAGE_VERSION% 
+dotnet pack MemoryPools.Collections\MemoryPools.Collections.csproj -o MemoryPools.Collections\bin\Release -c Release /p:Version=%PACKAGE_VERSION%
+
+dotnet nuget push MemoryPools\bin\Release\MemoryPools.%PACKAGE_VERSION%.nupkg -k oy2cthywk3rsieisauhabiru7magfuue3nm4xr4illfuuq -s https://api.nuget.org/v3/index.json
+dotnet nuget push MemoryPools.Collections\bin\Release\MemoryPools.Collections.%PACKAGE_VERSION%.nupkg -k oy2cthywk3rsieisauhabiru7magfuue3nm4xr4illfuuq -s https://api.nuget.org/v3/index.json
